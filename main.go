a602d0a65cc5c6fc251f8e7fbe0cf630320d1608
alfaysal <all.faysal@pathao.com>
2024-10-08 17:23:42 +0600

    play-with-go command is working.

--- main.go
+++ main.go
@@ -1,21 +1,11 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"fmt"
-	"github.com/alfaysal/play-with-go/creature"
-)
-
-// run the imported creature package first then inside the calle function.
-
-func init() {
-	fmt.Println("Initializing from main first")
-}
-
-func init() {
-	fmt.Println("Initializing from main second")
-}
+import "github.com/alfaysal/play-with-go/cmd"
 
 func main() {
-	fmt.Println("Hello World")
-	fmt.Println(creature.Creatures)
+	cmd.Execute()
 }
